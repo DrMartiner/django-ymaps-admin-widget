55fac26c398c65b1ed64166d799bb11bd499fd8a
DrMartiner <DrMartiner@GMail.Com>
2013-08-04 16:54:40 +0700

    fix: removed ready

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import os
 from distutils.core import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
